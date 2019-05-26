00372ee3ed71fbcbc0fcb9a341ee713c840f3820
jacobcoxndsu <jacob.cox@ndsu.edu>
2019-05-25 20:30:52 -0500

    Physics
    Physics is not working as desired, however it is in an interesting state, I am saving this "snapshot".

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ var tileWidth = c.tileWidth;
 var tileHeight = c.tileHeight;
 
 //Create the rectangle for the quadtree
-var rectangle = new QuadTreeModule.Rectangle((mapWidth * tileWidth) / 2, (mapHeight * tileHeight) / 2, (mapWidth * tileWidth) / 2, (mapHeight * tileHeight) / 2,);
+var rectangle = new QuadTreeModule.Rectangle((mapWidth * tileWidth) / 2, (mapHeight * tileHeight) / 2, (mapWidth * tileWidth) / 2, (mapHeight * tileHeight) / 2, );
 
 //Create the quadtree
 var QUADTREE = new QuadTreeModule.QuadTree(rectangle, 10);
@@ -40,7 +40,7 @@ app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(gameport);
 
-Log("app", "Server Started", "info", true);
+Log("app", "Server Started on port: " + gameport, "info", true);
 
 var SOCKET_LIST = {};
 var PLAYER_LIST = [];
@@ -107,15 +107,14 @@ io.sockets.on('connection', function (socket) {
 
         //Delete the cells when the player leaves
         var tempList = [];
-        for(var i in CELL_LIST)
-        {
-            if(CELL_LIST[i].id != socket.id){
+        for (var i in CELL_LIST) {
+            if (CELL_LIST[i].id != socket.id) {
                 tempList.push(CELL_LIST[i]);
             }
         }
 
         CELL_LIST.length = 0;
-        for(var i in tempList){
+        for (var i in tempList) {
             var c = tempList[i];
             CELL_LIST.push(c);
         }
@@ -140,8 +139,8 @@ io.sockets.on('connection', function (socket) {
             player.pressingDown = data.state;
         } else if (data.inputId === 'space') {
             player.pressingSpace = data.state;
-            for(var i in CELL_LIST){
-                if(CELL_LIST[i].id == socket.id){
+            for (var i in CELL_LIST) {
+                if (CELL_LIST[i].id == socket.id) {
                     CELL_LIST[i].mass++;
                 }
             }
@@ -163,16 +162,26 @@ io.sockets.on('connection', function (socket) {
         player.mouseDown = data.state;
         player.mouseSelectFirstX = data.x;
         player.mouseSelectFirstY = data.y;
+
+        for(var i in CELL_LIST){
+            var cell = CELL_LIST[i];
+
+            if(cell.selected){
+                cell.tx = data.x + player.canvasXZero;
+                cell.ty = data.y + player.canvasYZero;
+                cell.target = true;
+            }
+        }
     });
 
     //When the player clicks the mouse down.
     socket.on('rightmousedown', function (data) {
         //player.rightclicked(CELL_LIST, BLOB_LIST, data.x, data.y);
 
-        for(var i in CELL_LIST){
+        for (var i in CELL_LIST) {
             var cell = CELL_LIST[i];
 
-            if(cell.selected){
+            if (cell.selected) {
                 cell.vx = 1 * cell.x - (data.x + player.canvasXZero);
                 cell.vy = 1 * cell.y - (data.y + player.canvasYZero);
             }
@@ -185,10 +194,9 @@ io.sockets.on('connection', function (socket) {
         player.mouseSelectSecondX = data.x;
         player.mouseSelectSecondY = data.y;
 
-        for(var i in BLOB_LIST){
+        for (var i in BLOB_LIST) {
             var blobi = BLOB_LIST[i]
-            for(var j in blobi)
-            {
+            for (var j in blobi) {
                 var b = blobi[j];
                 b.selected = false;
             }
@@ -198,20 +206,20 @@ io.sockets.on('connection', function (socket) {
     });
 });
 
-function doCirclesOverlap(x1, y1, r1, x2, y2, r2){
+function doCirclesOverlap(x1, y1, r1, x2, y2, r2) {
     return Math.abs((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) < (r1 + r2) * (r1 + r2);
 }
 
-function getDistance(x1, y1, r1, x2, y2, r2){
+function getDistance(x1, y1, r1, x2, y2, r2) {
     return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) || 1;
 }
 
-function collider(){
+function collider() {
 
     var collidedParis = [];
 
     //STATIC RESOLUTION
-    for(var i in CELL_LIST){
+    for (var i in CELL_LIST) {
         var cell = CELL_LIST[i];
 
         //Get nearby Objects for collision
@@ -223,13 +231,13 @@ function collider(){
         var overlap = 0;
 
         //Go through each object in the range and perform collision calculations
-        for(var j in targets){
+        for (var j in targets) {
             var target = targets[j].data;
 
             //Check to make sure the cell and the target are not the same
-            if(cell.uniqueID != target.uniqueID){
+            if (cell.uniqueID != target.uniqueID) {
                 //Check if the objects overlap
-                if(doCirclesOverlap(cell.x, cell.y, cell.size, target.x, target.y, target.size)){
+                if (doCirclesOverlap(cell.x, cell.y, cell.size, target.x, target.y, target.size)) {
 
                     //Add both cells to the colliding pairs array for dynamic resolution later
                     collidedParis.push(cell);
@@ -245,34 +253,146 @@ function collider(){
 
                     //Resolve Target Collision  
                     target.x += overlap * (cell.x - target.x) / distance;
-                    target.y += overlap * (cell.y - target.y) / distance;              
+                    target.y += overlap * (cell.y - target.y) / distance;
                 }
             }
         }
     }
 
     //DYNAMIC RESOLUTION
-    for(var i = 0; i < collidedParis.length; i += 2){
+    for (var i = 0; i < collidedParis.length; i += 2) {
         var cell1 = collidedParis[i];
         var cell2 = collidedParis[i + 1];
 
+        //Get the distance between the two balls
+        distance = getDistance(cell1.x, cell1.y, cell1.size, cell2.x, cell2.y, cell2.size);
 
+        //get the normal vector
+        var nx = (cell2.x - cell1.x) / distance;
+        var ny = (cell2.y - cell1.y) / distance;
+
+        //tangent
+        var tx = -ny;
+        var ty = nx;
+
+        //Dot product tangent
+        var dptan1 = cell1.vx * tx + cell1.vy * ty;
+        var dptan2 = cell2.vx * tx + cell2.vy * ty;
+
+        //Dot Product Normal
+        var dpnorm1 = cell1.vx * nx + cell1.vy * ny;
+        var dpnorm2 = cell2.vx * nx + cell2.vy * ny;
+
+        //Conservation of momentum in 1D
+        var m1 = (dpnorm1 * (cell1.mass - cell2.mass) + 2 * cell2.mass * dpnorm2) / (cell1.mass + cell2.mass);
+        var m2 = (dpnorm2 * (cell2.mass - cell2.mass) + 2 * cell1.mass * dpnorm1) / (cell1.mass + cell2.mass);
+
+        //Update the Cells Velocities
+        cell1.vx = tx * dptan1 + nx * m1;
+        cell1.vy = ty * dptan1 + ny * m1;
+        cell2.vx = tx * dptan2 + nx * m2;
+        cell2.vy = ty * dptan2 + ny * m2;
     }
 
 }
 
-var lastFrameTimeMs = 0;
-var maxFPS = 60;
+function tick(dt) {
+    //Create the rectangle for the quadtree
+    var rectangle = new QuadTreeModule.Rectangle((mapWidth * tileWidth) / 2, (mapHeight * tileHeight) / 2, (mapWidth * tileWidth) / 2, (mapHeight * tileHeight) / 2, );
+    //Create the quadtree
+    QUADTREE = new QuadTreeModule.QuadTree(rectangle, 10);
 
-function loop(){
+    //For every player in the PLAYER_LIST
+    for(var p in PLAYER_LIST){
+        var player = PLAYER_LIST[p];
+        var socket = SOCKET_LIST[player.socket_id];
+
+        //Update the players location on there screen
+        socket.emit('updateLocation', player.getInfo());
+
+        //array to package the cells into
+        var cells = [];
+
+        //loop through all the cells
+        for (var c in CELL_LIST) {
+            var cell = CELL_LIST[c];
+            cell.update(dt);
+            cells.push(cell.getInfo());
+            //Add the cells to the quadtree
+            var point = new QuadTreeModule.Point(cell.x, cell.y, cell);
+            QUADTREE.insert(point);
+        }
+
+        socket.emit('cells', cells);
+
+        //Update the player
+        player.updatePosition();
+    }
 
 }
 
+/*
+---------- MAIN GAME LOOP ----------
+*/
+
+//Loop variables
+var hrstart = process.hrtime();
+var desiredTicks = 1000 / 60;
+var dt = 0;
+
+//Average Calculation variables
+var startTime = 0;
+var endTime = 0;
+var totalTime = 0;
+var numberOfLoops = 0;
+
+setInterval(function (argument) {
+    // execution time simulated with setTimeout function
+    hrend = process.hrtime(hrstart);
+
+    //grab the current time
+    if(DEBUG){
+        startTime = process.hrtime()[1];
+    }
+
+    //Calculate the delta time in miliseconds
+    dt = hrend[1] / 1000000000;
+
+    collider();
+
+    //Perform game operations
+    tick(dt);
+
+    //Reset the time
+    hrstart = process.hrtime();
+
+
+    if (DEBUG) {
+
+        //grab the end time
+        endTime = process.hrtime()[1];
+
+        //calculate the time the loop took and add it to the total
+        totalTime = totalTime + Math.abs(endTime - startTime);
+
+        if (numberOfLoops >= 3600) {
+            Log("app", "Average Loop Execution Time " + (totalTime / numberOfLoops) / 1000000 + "ms", "", false);
+            totalTime = 0;
+            numberOfLoops = 0;
+        }
+
+        //Increment the loops variable
+        numberOfLoops++;
+    }
+
+}, desiredTicks);
+
+/*
 //The current "game loop" -This needs to be updated later to be more functional. 
 setInterval(function () {
 
     //Create the rectangle for the quadtree
-    var rectangle = new QuadTreeModule.Rectangle((mapWidth * tileWidth) / 2, (mapHeight * tileHeight) / 2, (mapWidth * tileWidth) / 2, (mapHeight * tileHeight) / 2,);
+    var rectangle = new QuadTreeModule.Rectangle((mapWidth * tileWidth) / 2, (mapHeight * tileHeight) / 2, (mapWidth * tileWidth) / 2, (mapHeight * tileHeight) / 2, );
     //Create the quadtree
     QUADTREE = new QuadTreeModule.QuadTree(rectangle, 10);
 
@@ -295,9 +415,9 @@ setInterval(function () {
 
         var blobs = [];
 
-        for(var b1 in BLOB_LIST){
+        for (var b1 in BLOB_LIST) {
             var blobArray = BLOB_LIST[b1];
-            for(var b2 in blobArray){
+            for (var b2 in blobArray) {
                 //var blob = blobArray[b2];
                 //blob.update();
                 //blobs.push(blob.getInfo());
@@ -330,10 +450,12 @@ setInterval(function () {
                     }
                 }
             }
-        }*/
+        }
 
         socket.emit('blobs', blobs);
 
         player.updatePosition();
     }
 }, 1000 / 60); //60 times a second
+
+*/
--- server/js/cell.js
+++ server/js/cell.js
@@ -9,7 +9,8 @@ class Cell {
         //size
         this.size = 40;
         //mass
-        this.mass = 40;
+        this.mass = this.size * 2;
+        this.maxSpeed = 20;
         //color
         this.color = "blue";
         //Selected or not
@@ -26,6 +27,8 @@ class Cell {
         //target position
         this.tx = this.x;
         this.ty = this.y;
+        //target aquired
+        this.target = false;
 
         this.counterMax = 100;
         this.counter = this.counterMax;
@@ -33,28 +36,98 @@ class Cell {
 
     update(dt) {
 
-        if(!dt){
+        if (!dt) {
             dt = 1;
         }
 
-        //Apply Friction
-        this.ax = -this.vx * 0.8;
-        this.ay = -this.vy * 0.8;
+        var dx = 0;
+        var dy = 0;
 
-        //Update the velocity with the acceleration
-        this.vx += this.ax * dt;
-        this.vy += this.ay * dt;
+        if (this.target) {
 
-        //Update the position with the velocity
-        this.x += this.vx * dt;
-        this.y += this.vy * dt;
+            var distance = Math.sqrt((this.tx - this.x) * (this.tx - this.x) + (this.ty - this.y) * (this.ty - this.y));
 
-        //Clamp the velocity to 0 if it gets too small
-        if(Math.abs(this.vx * this.vx + this.vy * this.vy) <= 0.01){
-            this.vx = 0;
-            this.vy = 0;
+            var targetX = this.tx - this.x;
+            var targetY = this.ty - this.y;
+
+            targetX = (targetX * 10) / distance;
+            targetY = (targetY * 10) / distance;
+
+            //console.log(targetX + "  " + targetY);
+
+            this.ax = targetX * Math.abs(this.tx - this.x);
+            this.ay = targetY * Math.abs(this.ty - this.y);
+
+            console.log(this.ax + "  " + this.ay + "  " + dt);
+
+            //Update Velocity
+            this.vx += this.ax * dt;
+            this.vy += this.ay * dt;
+
+            //console.log(this.vx + "  " + this.vy);
+
+            //Update the position with the velocity
+            this.x += (this.vx * dt);
+            this.y += (this.vy * dt);
+
+            //console.log(this.x + "  " + this.y + "  " + dt);
+
+            //Clamp the velocity to 0 if it gets too small
+            if (Math.abs(this.vx * this.vx + this.vy * this.vy) <= 0.5) {
+                this.vx = 0;
+                this.vy = 0;
+            }
+
+
+
+
+
+            /*
+            //Calculations if there is a target
+            var distance = Math.sqrt((this.tx - this.x) * (this.tx - this.x) + (this.ty - this.y) * (this.ty - this.y));
+            dx = (this.tx - this.x) / distance;
+            dy = (this.ty - this.y) / distance;
+
+            this.vx += dx * 100 * dt;
+            this.vy += dy * 100 * dt;
+
+            this.x += (this.vx * dt);
+            this.y += (this.vy * dt);
+
+            //Clamp the velocity to 0 if it gets too small
+            if (Math.abs(this.vx * this.vx + this.vy * this.vy) <= 0.5) {
+                this.vx = 0;
+                this.vy = 0;
+            }
+            */
+
+            //Clamp the target distance
+            if (distance < 1) {
+                this.x = this.tx;
+                this.y = this.ty;
+                this.target = false;
+            }
+        } else {
+            //Calculations when there is not a target
+
+            //Apply Friction
+            this.ax = -this.vx * 2;
+            this.ay = -this.vy * 2;
+
+            //Update Velocity
+            this.vx += this.ax * dt;
+            this.vy += this.ay * dt;
+
+            //Update the position with the velocity
+            this.x += (this.vx * dt);
+            this.y += (this.vy * dt);
+
+            //Clamp the velocity to 0 if it gets too small
+            if (Math.abs(this.vx * this.vx + this.vy * this.vy) <= 0.5) {
+                this.vx = 0;
+                this.vy = 0;
+            }
         }
-
 
         //If the position and the target are not the same, go to the target.
         /*if (this.tx != this.x || this.ty != this.y) {
